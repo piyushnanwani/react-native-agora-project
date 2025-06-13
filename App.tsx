7c5c69fe93df5871dc2889feec724477fdfcbd95
Piyush Nanwani <pnanwani61@gmail.com>
2025-06-13 23:28:11 +0530

    implemented agora chat sdk

--- App.tsx
+++ App.tsx
@@ -3,115 +3,325 @@
  * https://github.com/facebook/react-native
  *
  * @format
+ * @flow strict-local
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+// Import depend packages.
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ChatClient,
+  ChatOptions,
+  ChatMessageChatType,
+  ChatMessage,
+} from 'react-native-agora-chat';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+// Defines the App object.
+const App = () => {
+  // Defines the variable.
+  const title = 'WhatsApp Clone - React Native';
+  // Replaces <your appKey> with your app key.
+  const appKey = '611355963#1562506';
+  // Replaces <your userId> with your user ID.
+  const [username, setUsername] = React.useState('piyush');
+  // Replaces <your agoraToken> with your Agora token.
+  const [chatToken, setChatToken] = React.useState(
+    '007eJxTYJg95eGTr9uvtfr/MVrWdf7vy4f2TSbnWyMsLh4QlThmHxytwGBkkGSabGGZbJZqZGCSkmhhkWaUaGiaaGqWkmycbJGaymnkk9EQyMjw/PJOZkYGVgZGIATxVRhSEg3NEy3MDHRNLEyTdQ0N0wx0Ey3SzHWNjC3TDCwNk0xSjEwA6ncqLg==',
   );
-}
+  const [targetId, setTargetId] = React.useState('');
+  const [content, setContent] = React.useState('');
+  const [logText, setWarnText] = React.useState('Show log area');
+  const chatClient = ChatClient.getInstance();
+  const chatManager = chatClient.chatManager;
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // Outputs console logs.
+  useEffect(() => {
+    logText.split('\n').forEach((value, index, array) => {
+      if (index === 0) {
+        console.log(value);
+      }
+    });
+  }, [logText]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // Outputs UI logs.
+  const rollLog = text => {
+    setWarnText(preLogText => {
+      let newLogText = text;
+      preLogText
+        .split('\n')
+        .filter((value, index, array) => {
+          if (index > 8) {
+            return false;
+          }
+          return true;
+        })
+        .forEach((value, index, array) => {
+          newLogText += '\n' + value;
+        });
+      return newLogText;
+    });
   };
 
+  useEffect(() => {
+    // Registers listeners for messaging.
+    const setMessageListener = () => {
+      let msgListener = {
+        onMessagesReceived(messages) {
+          for (let index = 0; index < messages.length; index++) {
+            rollLog('received message is : ' + messages[index]?.body?.content  );
+          }
+        },
+        onCmdMessagesReceived: messages => {},
+        onMessagesRead: messages => {},
+        onGroupMessageRead: groupMessageAcks => {},
+        onMessagesDelivered: messages => {},
+        onMessagesRecalled: messages => {},
+        onConversationsUpdate: () => {},
+        onConversationRead: (from, to) => {},
+      };
+
+      chatManager.removeAllMessageListener();
+      chatManager.addMessageListener(msgListener);
+    };
+
+    // Initializes the SDK.
+    // Initializes any interface before calling it.
+    const init = () => {
+      let o = new ChatOptions({
+        autoLogin: false,
+        appKey: appKey,
+      });
+      chatClient.removeAllConnectionListener();
+      chatClient
+        .init(o)
+        .then(() => {
+          rollLog('init success');
+          let listener = {
+            onTokenWillExpire() {
+              rollLog('token expire.');
+            },
+            onTokenDidExpire() {
+              rollLog('token did expire');
+            },
+            onConnected() {
+              rollLog('onConnected');
+              setMessageListener();
+            },
+            onDisconnected(errorCode) {
+              rollLog('onDisconnected:' + errorCode);
+            },
+          };
+          chatClient.addConnectionListener(listener);
+        })
+        .catch(error => {
+          rollLog(
+            'init fail: ' +
+              (error instanceof Object ? JSON.stringify(error) : error),
+          );
+        });
+    };
+
+    init();
+  }, [chatClient, chatManager, appKey]);
+
+  // Logs in with an account ID and a token.
+  const login = () => {
+    rollLog('start login ...');
+    chatClient
+      .loginWithToken(username, chatToken)
+      .then(() => {
+        rollLog('login operation success.');
+      })
+      .catch(reason => {
+        rollLog('login fail: ' + JSON.stringify(reason));
+      });
+  };
+
+  // Logs out from server.
+  const logout = () => {
+    rollLog('start logout ...');
+    chatClient
+      .logout()
+      .then(() => {
+        rollLog('logout success.');
+      })
+      .catch(reason => {
+        rollLog('logout fail:' + JSON.stringify(reason));
+      });
+  };
+
+  // Sends a text message to somebody.
+  const sendmsg = () => {
+    let msg = ChatMessage.createTextMessage(
+      targetId,
+      content,
+      ChatMessageChatType.PeerChat,
+    );
+    const callback = new (class {
+      onProgress(locaMsgId, progress) {
+        rollLog(`send message process: ${locaMsgId}, ${progress}`);
+      }
+      onError(locaMsgId, error) {
+        rollLog(`send message fail: ${locaMsgId}, ${JSON.stringify(error)}`);
+      }
+      onSuccess(message) {
+        rollLog('send message success: ' + message.localMsgId);
+      }
+    })();
+    rollLog('start send message ...');
+    chatClient.chatManager
+      .sendMessage(msg, callback)
+      .then(() => {
+        rollLog('send message: ' + msg.localMsgId);
+      })
+      .catch(reason => {
+        rollLog('send fail: ' + JSON.stringify(reason));
+      });
+  };
+
+  // Renders the UI.
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.inputCon}>
+          <TextInput
+            multiline
+            style={styles.inputBox}
+            placeholder="Enter username"
+            onChangeText={text => setUsername(text)}
+            value={username}
+            autoCapitalize={'none'}
+          />
+        </View>
+        <View style={styles.inputCon}>
+          <TextInput
+            multiline
+            style={styles.inputBox}
+            placeholder="Enter chatToken"
+            onChangeText={text => setChatToken(text)}
+            value={chatToken}
+            autoCapitalize={'none'}
+          />
+        </View>
+        <View style={styles.buttonCon}>
+          <Text style={styles.eachBtn} onPress={login}>
+            SIGN IN
+          </Text>
+          <Text style={styles.eachBtn} onPress={logout}>
+            SIGN OUT
+          </Text>
+        </View>
+        <View style={styles.inputCon}>
+          <TextInput
+            multiline
+            style={styles.inputBox}
+            placeholder="Enter the username you want to send"
+            onChangeText={text => setTargetId(text)}
+            value={targetId}
+            autoCapitalize={'none'}
+          />
+        </View>
+        <View style={styles.inputCon}>
+          <TextInput
+            multiline
+            style={styles.inputBox}
+            placeholder="Enter content"
+            onChangeText={text => setContent(text)}
+            value={content}
+            autoCapitalize={'none'}
+          />
+        </View>
+        <View style={styles.buttonCon}>
+          <Text style={styles.btn2} onPress={sendmsg}>
+            SEND TEXT
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.logText}>{logText}</Text>
+        </View>
+        <View>
+          <Text style={styles.logText}>{}</Text>
+        </View>
+        <View>
+          <Text style={styles.logText}>{}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
+// Sets UI styles.
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  titleContainer: {
+    height: 60,
+    backgroundColor: '#6200ED',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
+  title: {
+    lineHeight: 60,
+    paddingLeft: 15,
+    color: '#fff',
+    fontSize: 20,
     fontWeight: '700',
+  },
+  inputCon: {
+    marginLeft: '5%',
+    width: '90%',
+    height: 60,
+    paddingBottom: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  inputBox: {
+    marginTop: 15,
+    width: '100%',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  buttonCon: {
+    marginLeft: '2%',
+    width: '96%',
+    flexDirection: 'row',
+    marginTop: 20,
+    height: 26,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  eachBtn: {
+    height: 40,
+    width: '28%',
+    lineHeight: 40,
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+    backgroundColor: '#6200ED',
+    borderRadius: 5,
+  },
+  btn2: {
+    height: 40,
+    width: '45%',
+    lineHeight: 40,
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+    backgroundColor: '#6200ED',
+    borderRadius: 5,
+  },
+  logText: {
+    padding: 10,
+    marginTop: 10,
+    color: '#ccc',
+    fontSize: 14,
+    lineHeight: 20,
   },
 });
 
